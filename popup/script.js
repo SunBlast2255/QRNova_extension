3dbd00a527564104419f3bb4f8a0c7e3e60543c4
SunBlast2215 <sunflash3412@gmail.com>
2024-08-31 23:27:16 +0300

    Settings done

--- popup/script.js
+++ popup/script.js
@@ -1,15 +1,40 @@
-function generate(data){
+async function generate(data){
     document.getElementById("qrcode").innerHTML = "";
+
+    let settings = await getSettings();
 
     let qr = new QRCode(document.getElementById("qrcode"), {
         text: data,
-        width: 256,
-        height: 256,
-        colorDark : "#000000",
-        colorLight : "#ffffff",
+        width: parseInt(settings.width),
+        height: parseInt(settings.height),
+        colorDark : settings.foreground,
+        colorLight : settings.background,
         correctLevel : QRCode.CorrectLevel.H
     });
 }
+
+async function getSettings(){
+    return new Promise((resolve, reject) => {
+        chrome.storage.local.get(null, function(result) {
+            if (chrome.runtime.lastError) {
+                return reject(chrome.runtime.lastError);
+            }
+            
+            if (Object.keys(result).length === 0) {
+                let defaultSettings = {background: "#FFFFFF", foreground: "#000000", width: "256", height: "256"};
+                chrome.storage.local.set(defaultSettings, function() {
+                    resolve(defaultSettings);
+                });
+            } else {
+                resolve(result);
+            }
+        });
+    });
+}
+
+window.onload = function(e) {
+    generate("Check my other projects: https://github.com/SunBlast2255");
+};
 
 document.getElementById("generate-btn").addEventListener("click", function() {
     let data = document.getElementById("url-input").value;
@@ -17,15 +42,39 @@ document.getElementById("generate-btn").addEventListener("click", function() {
     generate(data);
 });
 
-window.onload = function(e) {
-    generate("Check my other projects: https://github.com/SunBlast2255")
-};
+//settings
 
-window.addEventListener('click', function(){
-    document.getElementById("context").style.display = "none";
+document.getElementById("settings-btn").addEventListener("click", async function() {
+    document.getElementById("settings-modal").style.display = "flex";
+
+    let settings = await getSettings();
+
+    document.getElementById("background").value = settings.background;
+    document.getElementById("foreground").value = settings.foreground;
+    document.getElementById("width").value = settings.width;
+    document.getElementById("height").value = settings.height;
 });
 
-window.oncontextmenu = function (e){
+document.getElementById("apply-btn").addEventListener("click", function() {
+    document.getElementById("settings-modal").style.display = "none";
+
+    let background = document.getElementById("background").value;
+    let foreground = document.getElementById("foreground").value;
+    let width = document.getElementById("width").value;
+    let height = document.getElementById("height").value;
+
+    chrome.storage.local.set({
+        background: background,
+        foreground: foreground,
+        width: width,
+        height: height
+    });
+    
+});
+
+//context menu
+
+document.getElementById("qrcode").oncontextmenu = function (e){
     e.preventDefault();
 
     let context = document.getElementById("context");
@@ -72,5 +121,9 @@ document.getElementById("copy").addEventListener("click", function() {
         const item = new ClipboardItem({"image/png": blob});
         navigator.clipboard.write([item]);
     });
+});
+
+window.addEventListener('click', function(){
+    document.getElementById("context").style.display = "none";
 });
 
